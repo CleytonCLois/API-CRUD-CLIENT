83e77875a404835e4b86fa18a0a918c504256e10
Cleyton Corrêa Lois <cleytonclois@gmail.com>
2021-02-28 15:18:24 -0300

    Alteração string conexão

--- infraestrutura/conexao.js
+++ infraestrutura/conexao.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql')
 
 const conexao = mysql.createConnection({
-    host: 'us-cdbr-east-03.cleardb.com',
-    port: '3306',
-    user: 'b40fa5026af7f0',
-    password: 'f5513488',
-    database: 'heroku_f612ed612a0f202'
+    host: 'localhost',
+    port: '3307',
+    user: 'root',
+    password: 'password',
+    database: 'api'
 })
 
 module.exports = conexao
